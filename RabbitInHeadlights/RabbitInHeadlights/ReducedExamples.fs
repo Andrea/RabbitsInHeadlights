9bde828d7cd40857d7bb1fbd5b5ee7276967fc61
Andrea <roundcrisis@gmail.com>
2014-10-29 12:48:42 +0100

    improved on the type providers sample

---
+++ RabbitInHeadlights/RabbitInHeadlights/ReducedExamples.fs
@@ -0,0 +1,37 @@
+﻿module ReducedExamples
+    type Key = {key : string}
+    type KeyboardInput = 
+        member this.KeyPressed (key:Key) =
+            if key.key = "Left" then 
+                true
+            else
+                false
+(*
+    let (|LeftKey|RightKey|OtherKey|) (keyboard:KeyboardInput) = 
+        if keyboard.KeyPressed(Key.Left) then LeftKey
+        elif keyboard.KeyPressed(Key.Right) then RightKey
+        else OtherKey "Hi, you pressed a key...well that is interesting :D"
+            
+    
+    let (|RightSoemthing|) (keyboard:KeyboardInput) = 
+        keyboard.KeyPressed(Key.Right), "Test from right something" 
+
+    let (|LeftSoemthing|) (keyboard:KeyboardInput) = 
+        keyboard.KeyPressed(Key.Left)
+    
+    interface ICmpUpdatable with
+        member this.OnUpdate()=        
+            match DualityApp.Keyboard with
+            //| LeftSoemthing true & RightSoemthing (wasPressed, stringy) -> () //the left and not the right
+            | LeftKey  -> 
+                let leftWall = Scene.Current.FindGameObject("LeftWall")
+                if leftWall.Transform.Pos.X + HalfWidth leftWall.RigidBody <= this.GameObj.Transform.Pos.X - HalfWidth this.GameObj.RigidBody  then
+                    this.GameObj.Transform.MoveBy(-Vector2.UnitX * 10.0f)                  
+            | RightKey->
+                 let rightWall = Scene.Current.FindGameObject("RightWall")
+                 if (this.GameObj.Transform.Pos.X + HalfWidth this.GameObj.RigidBody <= rightWall.Transform.Pos.X - HalfWidth rightWall.RigidBody) then
+               //if (rightWall.Transform.Pos.X - HalfWidth rightWall.RigidBody > this.GameObj.Transform.Pos.X + HalfWidth this.GameObj.RigidBody ) then
+                    this.GameObj.Transform.MoveBy(Vector2.UnitX * 10.0f)
+            | OtherKey s-> ()
+
+            *)
--- RabbitInHeadlights/RabbitInHeadlights/TypeProviderExample.fs
+++ RabbitInHeadlights/RabbitInHeadlights/TypeProviderExample.fs
@@ -6,30 +6,17 @@
 #r "PresentationFramework.dll"
 #r "System.Xaml"
 #r @"C:\Source\Rabbits\RabbitInHeadlights\packages\FSharp.Data.2.0.8\lib\portable-net40+sl5+wp8+win8\FSharp.Data.dll"
- 
+#I "packages/FSharp.Charting.0.90.7"
+#load @"C:\Source\RabbitsInHeadlights\RabbitInHeadlights\packages\FSharp.Charting.0.90.7\FSharp.Charting.fsx"
 #endif
 open System
 open FSharp.Data
+open FSharp.Charting
 open System.IO
  
  [<Literal>]
 let path = "C:\Source\Rabbits\IrelandProgrammingLanguages.csv"
 type Languages = FSharp.Data.CsvProvider<path>
- 
-let split (text:string) =
-    text.Split ','
-    |> Seq.toList
- 
-let splitAndCount string  =
-    let languages= split string
-    float languages.Length
- 
-    
-let listLength languages=
-    List.length languages
-    
-let reduceLanguageCount langs count=
-    count, List.fold (fun acc langCount -> if(int langCount =count) then acc + 1 else acc ) 0 langs
     
 let readLanguageData =     
     
@@ -39,6 +26,16 @@ let readLanguageData =
  
     printfn "Number of languages in production vs hobby"
     
+    let split (text:string) =
+        text.Split ','
+        |> Seq.toList
+ 
+    let splitAndCount string  =
+        let languages= split string
+        float languages.Length
+    
+    let reduceLanguageCount langs count=
+        count, List.fold (fun acc langCount -> if(int langCount =count) then acc + 1 else acc ) 0 langs
     let languageCount = Seq.toList data.Rows
                         |> List.map ( fun r -> splitAndCount r.``What programming language(s) are you using in production?``)     
     let reducedCount = [for x in 1..7 -> reduceLanguageCount languageCount x]    
@@ -48,5 +45,25 @@ let readLanguageData =
                         |> List.map ( fun r -> splitAndCount r.``What programming language(s) are you using in your hobby/side projects?``)     
     let hobbiesReducedCount = [for x in 1..7 -> reduceLanguageCount hobbiesCount x]    
     printfn "Number of languges used in hobbies %A" hobbiesReducedCount
+    
+    let plot = hobbiesReducedCount 
+                |> Chart.Line
+                |> Chart.WithTitle "Languages used in production"
+                |> Chart.WithYAxis( Title = "devs") 
+                |> Chart.WithXAxis( Title = "language count") 
+    
+    
+    let mostUsedLanguages =  data.Rows
+                             |> Seq.map (fun r-> r.``What programming language(s) are you using in production?``.Split ',' )
+                             |> Seq.append (data.Rows |> Seq.map (fun r-> r.``What programming language(s) are you using in your hobby/side projects?``.Split ','))
+                             |> Seq.collect (fun row -> row |> Seq.ofArray )                             
+                             |> Seq.countBy (fun x-> x.ToLower())                             
+                             
+                             |> Seq.sortBy(fun x-> -snd x)                             
+                             |> Seq.take 6
+                             |> Chart.Bar
+                             |> Chart.WithXAxis(LabelStyle=new ChartTypes.LabelStyle() 
+
+    0
  
  
